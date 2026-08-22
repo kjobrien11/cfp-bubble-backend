597028c675813743b4815f73d465d2105f016c2a
KJ OBrien <kjokjo219@gmail.com>
2026-08-22 12:45:48 -0400

    production changes

--- cfpbubble/src/main/java/com/cfpbubble/cfpbubble/config/SecurityConfig.java
+++ cfpbubble/src/main/java/com/cfpbubble/cfpbubble/config/SecurityConfig.java
@@ -2,16 +2,9 @@ package com.cfpbubble.cfpbubble.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.util.List;
 
 @Configuration
-@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
@@ -19,42 +12,12 @@ public class SecurityConfig {
             throws Exception {
 
         http
-                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
-                .csrf(csrf -> csrf.disable());
+                .csrf(csrf -> csrf.disable())
+                .cors(cors -> {})
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll()
+                );
 
         return http.build();
-    }
-
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-
-        CorsConfiguration configuration = new CorsConfiguration();
-
-        configuration.setAllowedOrigins(List.of(
-                "http://localhost:4200",
-                "http://localhost:4222",
-                "http://192.168.1.168:4222",
-                "https://cfpbubble.com"
-        ));
-
-        configuration.setAllowedMethods(List.of(
-                "GET",
-                "POST",
-                "PUT",
-                "DELETE",
-                "OPTIONS"
-        ));
-
-        configuration.setAllowedHeaders(List.of("*"));
-
-        UrlBasedCorsConfigurationSource source =
-                new UrlBasedCorsConfigurationSource();
-
-        source.registerCorsConfiguration(
-                "/**",
-                configuration
-        );
-
-        return source;
     }
 }
--- cfpbubble/src/main/java/com/cfpbubble/cfpbubble/entity/BubbleTeamId.java
+++ cfpbubble/src/main/java/com/cfpbubble/cfpbubble/entity/BubbleTeamId.java
@@ -10,6 +10,7 @@ import java.io.Serializable;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@EqualsAndHashCode
 public class BubbleTeamId implements Serializable {
 
     private Long bubbleId;
